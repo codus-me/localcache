58fdfb8fd2e3fddd638b90b176b4ff11a089d0a7
Codus <codus@codus.me>
2022-11-29 19:55:02 +0800

    optimize: use suite.Require().Equal instead suite.Equal

--- localcache_test.go
+++ localcache_test.go
@@ -17,17 +17,17 @@ func (suite *localcacheTestSuite) SetupTest() {
 }
 
 func (suite *localcacheTestSuite) TestLocalcacheGetNil() {
-	suite.Equal(nil, suite.cache.Get("not exist"))
+	suite.Require().Equal(nil, suite.cache.Get("not exist"))
 }
 func (suite *localcacheTestSuite) TestLocalcacheSetThenGet() {
 	suite.cache.Set("mykey", 1)
-	suite.Equal(1, suite.cache.Get("mykey"))
+	suite.Require().Equal(1, suite.cache.Get("mykey"))
 }
 func (suite *localcacheTestSuite) TestLocalcacheGetOutdatedData() {
 	suite.cache.Set("mykey", 1)
 	impl := suite.cache.(*cacheImpl)
 	impl.createdAt["mykey"] = &time.Time{}
-	suite.Equal(nil, suite.cache.Get("mykey"))
+	suite.Require().Equal(nil, suite.cache.Get("mykey"))
 }
 
 func TestLocalcacheTestSuite(t *testing.T) {
